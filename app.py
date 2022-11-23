b489c4637a2da8779f878a6386955ca9d9b0c85c
hhenriques1999 <hhenriques1999@gmail.com>
2022-11-22 22:21:10 -0300

    Remover CORS
    Não é necessário no frontend

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from templates.content.main import main
-from flask_cors import CORS
-
 
 app = Flask(__name__)
-CORS(app) # MUDAR DEPOIS
+
 app.register_blueprint(main)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
